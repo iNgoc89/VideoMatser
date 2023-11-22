c3c71ab82693b2b353480eeb8a094b396c9c4750
iNgoc89 <ngocxmht@gmail.com>
2023-11-22 11:32:51 +0700

    up

--- FFmpegWebAPI/Controllers/VideoController.cs
+++ FFmpegWebAPI/Controllers/VideoController.cs
@@ -4,6 +4,7 @@ using FFmpegWebAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel.Design;
+using System.Diagnostics;
 using static FFmpegWebAPI.Controllers.VideoController;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -89,45 +90,53 @@ namespace FFmpegWebAPI.Controllers
                 }
 
                 //Kiểm tra có file phù hợp để ghép hay không
-                long? idThuMucLay = ThuMucLay;
-                long? idThuMucLuu = ThuMucLuu;
-                if (idThuMucLay > 0 && idThuMucLuu > 0)
+                try
                 {
-                    var urlLay = _xmhtService.P_ThuMuc_LayTheoID(null, idThuMucLay).Result;
-                    var urlLuu = _xmhtService.P_ThuMuc_LayTheoID(null, idThuMucLuu).Result;
-
-                    long thuMucConId = 0;
-                    var idThuMucLuuLay = _xmhtService.P_ThuMuc_LayTheoThuMucCha(null, idThuMucLay, videoConcatRequest.CameraId.ToString(), ref thuMucConId);
-                    var urlLuuLay = _xmhtService.P_ThuMuc_LayTheoID(null, idThuMucLuuLay).Result;
-                    if (urlLay != null && urlLuu != null && idThuMucLuuLay > 0 && urlLuuLay != null)
+                    long? idThuMucLay = ThuMucLay;
+                    long? idThuMucLuu = ThuMucLuu;
+                    if (idThuMucLay > 0 && idThuMucLuu > 0)
                     {
-                        var checkFile = _workVideoService.CheckFile(videoConcatRequest.CameraId, urlLuuLay.DuongDan, videoConcatRequest.BeginDate, videoConcatRequest.EndDate);
-                        if (checkFile?.Length > 0)
-                        {
-                            kq = _iOTService.P_ConcatVideoCamera_Insert(videoConcatRequest.GID, videoConcatRequest.CameraId, videoConcatRequest.BeginDate, videoConcatRequest.EndDate);
-                            if (kq > 0)
-                            {
-                                var dateNow = DateTime.Now.ToString("yyyyMM");
-                                videoReturl.Id = kq;
-                                videoReturl.GID = videoConcatRequest.GID;
-                                videoReturl.UrlPath = $"~/{ThuMucVirtual}/{DateTime.Now.ToString("yyyyMM")}/{videoConcatRequest.GID}.mp4";
-                                videoReturl.ErrMsg = "Tạo lệnh ghép thành công!";
+                        var urlLay = _xmhtService.P_ThuMuc_LayTheoID(null, idThuMucLay).Result;
+                        var urlLuu = _xmhtService.P_ThuMuc_LayTheoID(null, idThuMucLuu).Result;
 
+                        long thuMucConId = 0;
+                        var idThuMucLuuLay = _xmhtService.P_ThuMuc_LayTheoThuMucCha(null, idThuMucLay, videoConcatRequest.CameraId.ToString(), ref thuMucConId);
+                        var urlLuuLay = _xmhtService.P_ThuMuc_LayTheoID(null, idThuMucLuuLay).Result;
+                        if (urlLay != null && urlLuu != null && idThuMucLuuLay > 0 && urlLuuLay != null)
+                        {
+                            var checkFile = _workVideoService.CheckFile(videoConcatRequest.CameraId, urlLuuLay.DuongDan, videoConcatRequest.BeginDate, videoConcatRequest.EndDate);
+                            if (checkFile?.Length > 0)
+                            {
+                                kq = _iOTService.P_ConcatVideoCamera_Insert(videoConcatRequest.GID, videoConcatRequest.CameraId, videoConcatRequest.BeginDate, videoConcatRequest.EndDate);
+                                if (kq > 0)
+                                {
+                                    var dateNow = DateTime.Now.ToString("yyyyMM");
+                                    videoReturl.Id = kq;
+                                    videoReturl.GID = videoConcatRequest.GID;
+                                    videoReturl.UrlPath = $"~/{ThuMucVirtual}/{DateTime.Now.ToString("yyyyMM")}/{videoConcatRequest.GID}.mp4";
+                                    videoReturl.ErrMsg = "Tạo lệnh ghép thành công!";
+
+                                    return new JsonResult(videoReturl);
+                                }
+
+
+                            }
+                            else
+                            {
+                                videoReturl.ErrMsg = "Không có video phù hợp. Xem lại thời điểm lấy video!";
                                 return new JsonResult(videoReturl);
                             }
+                        }
 
 
-                        }
-                        else
-                        {
-                            videoReturl.ErrMsg = "Không có video phù hợp. Xem lại thời điểm lấy video!";
-                            return new JsonResult(videoReturl);
-                        }
+
                     }
-
-
-
                 }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+              
 
 
 
